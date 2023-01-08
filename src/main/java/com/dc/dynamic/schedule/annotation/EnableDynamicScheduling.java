7bad3a00e7d1447ab23c33b504af0f3aa7a5d2b4
USER-20200810GJ\Administrator <13275768320@163.com>
2023-01-08 17:53:05 +0800

    代码优化

--- src/main/java/com/dc/dynamic/schedule/annotation/EnableDynamicScheduling.java
+++ src/main/java/com/dc/dynamic/schedule/annotation/EnableDynamicScheduling.java
@@ -1,6 +1,7 @@
 package com.dc.dynamic.schedule.annotation;
 
 import com.dc.dynamic.schedule.config.DynamicSchedulingAutoRegistryProcess;
+import com.dc.dynamic.schedule.web.DynamicScheduleControllerRegister;
 import org.springframework.context.annotation.Import;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
